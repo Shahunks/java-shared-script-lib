5b51cdb6a5d956e55d4bc3fea308a5fd4d4a7dc0
shank <shashank3419@gmail.com>
2023-09-04 16:45:06 +1200

    add-lib

--- src/com/example/mypackage/myscript.groovy
+++ src/com/example/mypackage/myscript.groovy
@@ -1,5 +1,10 @@
 package com.example.mypackage
 
-def sayHello(name) {
-    return "Hello, ${name}!"
+def myscript(String repoUrl, String targetDir) {
+    try {
+        echo "Cloning repository from ${repoUrl} to ${targetDir}"
+        sh "git clone ${repoUrl} ${targetDir}"
+    } catch (Exception e) {
+        error "Failed to clone repository from ${repoUrl}. Error: ${e.getMessage()}"
+    }
 }
