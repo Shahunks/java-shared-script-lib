b84f7dae71d678d60c6cd5a41d932da8dd052202
shank <shashank3419@gmail.com>
2023-09-05 01:11:46 +1200

    add-lib

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -22,7 +22,7 @@ def call(Map params) {
                 steps {
                     script{
                     println "Provisioning in ${env.environment}"
-                    sh 'cd terraform-aws/'
+                    sh 'cd /terraform-aws/'
                     sh 'ls'
                     sh 'terraform init'
                     }
