929037ab930de39f622ab0bba4bb56827d8c49b8
shank <shashank3419@gmail.com>
2023-09-05 18:53:30 +1200

    add-lib

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -6,7 +6,7 @@ def call(Map params) {
     env.repo = params.repo
     env.targetDir = params.branch
     env.environment = params.environment
-    def dirchange = "terraform-aws/$env.environment"
+    def dirChange = "terraform-aws/$env.environment"
     pipeline {
         agent any
         stages {
@@ -31,7 +31,7 @@ def call(Map params) {
                     println "Provisioning in ${env.environment}"
                     
                     sh '''
-                    cd $dirchange && terraform init 
+                    cd ${dirChange} && terraform init 
                     '''
                     }
                     }
