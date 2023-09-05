c3807f9e09ca9fd16c7c7449e9e4471b238d9456
shank <shashank3419@gmail.com>
2023-09-05 21:44:09 +1200

    add-lib

---
+++ vars/ECSFargatedeployment.groovy
@@ -0,0 +1,62 @@
+// myPipeline.groovy
+import com.example.pipeline.util.checkOut
+
+def call(Map params) {
+    env.repo = params.repo
+    env.targetDir = params.branch
+    env.environment = params.environment
+    def dirChange = "terraform-aws/$env.environment"
+    //env.awsAccountId = params.awsAccountId
+    env.version = params.version
+    pipeline {
+        agent any
+        
+        stages {
+        
+            stage('checkout') {
+                steps {
+                    script{
+                   new checkOut().call(params)
+                }
+                }
+            }
+            stage('Docker build') {
+                steps { 
+                    script{ 
+                    withAWS(credentials: 'AWS'){
+                    println "Building for ${env.environment}"            
+                    sh """ 
+                    aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 193566561588.dkr.ecr.ap-southeast-2.amazonaws.com
+                    docker build -t dx-test:${env.version} .
+                    docker tag test-dx:${env.version} 193566561588.dkr.ecr.ap-southeast-2.amazonaws.com/test-dx:${env.version}
+                    """
+                    }
+                    }
+                    }
+                }
+            stage('Docker push') {
+                steps {
+                    script {
+                    sh """docker push ${env.awsAccountId}.dkr.ecr.ap-southeast-2.amazonaws.com/test-dx:${env.version}"""
+                    }
+                }
+            }
+            // stage('Terraform apply') {
+            //     steps {
+            //         script{
+            //          def userInput = input(
+            //             id: 'terraform-apply',
+            //             message: 'Do you want to apply the Terraform plan?',
+            //             parameters: [booleanParam(defaultValue: false, description: 'Yes or No', name: 'APPROVE')]
+            //         )
+            //         if (userInput['APPROVE']) {
+            //             sh """cd ${dirChange} && terraform apply"""
+            //         } else {
+            //             error('Terraform apply was not approved by the user.')
+            //         }
+            //         }
+            //     }
+            // }
+        }
+    }
+}
