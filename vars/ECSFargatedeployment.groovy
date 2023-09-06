c4aac68959794ec985ae4fac32d6e8b3135f39b8
shank <shashank3419@gmail.com>
2023-09-06 20:36:13 +1200

    add-lib

--- vars/ECSFargatedeployment.groovy
+++ vars/ECSFargatedeployment.groovy
@@ -34,7 +34,9 @@ def call(Map params) {
             stage('Deploy') {
                 steps {
                     script {
+                    withAWS(credentials: 'AWS'){
                     new ecsDeployment().call()
+                    }
                     }
                 }
             }
