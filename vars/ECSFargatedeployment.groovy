097e6c57e5739202170085c07de91879d944fcea
shank <shashank3419@gmail.com>
2023-09-06 23:25:55 +1200

    add-lib

--- vars/ECSFargatedeployment.groovy
+++ vars/ECSFargatedeployment.groovy
@@ -34,15 +34,17 @@ def call(Map params) {
                 steps {
                     script {
                     withAWS(credentials: 'AWS'){
-                    sh "aws ecs describe-task-definition --task-definition  my-first-task --region ap-southeast-2  --output json > file.json"
+                   // sh "aws ecs describe-task-definition --task-definition  my-first-task --region ap-southeast-2  --output json > file.json"
                     //sh 'ls && pwd'
                     new ecsDeployment().call()
-                    sh """echo \$$$(cat file.json | jq 'del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) | del(.registeredAt)  | del(.registeredBy)') > file.json
-'
-                    aws ecs register-task-definition --family my-first-task --cli-input-json file://file.json --region ap-southeast-2 > /dev/null
+//                     sh """echo \$$$(cat file.json | jq 'del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) | del(.registeredAt)  | del(.registeredBy)') > file.json
+// '
+//                     aws ecs register-task-definition --family my-first-task --cli-input-json file://file.json --region ap-southeast-2 > /dev/null
 
-                    aws ecs update-service --service my-first-service --task-definition my-first-task --region ap-southeast-2 > /dev/null
-                    """
+//                     aws ecs update-service --service my-first-service --task-definition my-first-task --region ap-southeast-2 > /dev/null
+//                     """
+                    sh 'aws ecs update-service --cluster my-cluster --service my-first-service --force-new-deployment --region us-apsoutheast-2
+
                     }
                     }
                 }
