105892fcc780c660f0b156b66122447a52dfcb35
shank <shashank3419@gmail.com>
2023-09-06 20:44:20 +1200

    add-lib

--- src/com/example/pipeline/util/ecsDeployment.groovy
+++ src/com/example/pipeline/util/ecsDeployment.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonOutput
 
 def call() {
   
-sh 'aws ecs describe-task-definition --task-definition  my-first-task  > file.json'
+sh 'aws --profile default ecs describe-task-definition --task-definition  my-first-task  > file.json'
 def jsonFilePath = 'file.json' 
 def newImageValue = 'new-image-name:new-tag' 
 
@@ -16,6 +16,6 @@ def updatedJson = JsonOutput.toJson(jsonMap)
 
 new File(jsonFilePath).text = updatedJson
 
-sh 'aws ecs update-service --service my-first-service --task-definition my-first-task --cli-input-json file:file.json'
+sh 'aws --profile default ecs update-service --service my-first-service --task-definition my-first-task --cli-input-json file:file.json'
 
 }
