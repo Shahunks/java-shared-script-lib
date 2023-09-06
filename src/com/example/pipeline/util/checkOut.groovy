a4ae4777922e78925d52ecb001e4b8ba423e794c
shank <shashank3419@gmail.com>
2023-09-07 11:05:01 +1200

    add-lib

--- src/com/example/pipeline/util/checkOut.groovy
+++ src/com/example/pipeline/util/checkOut.groovy
@@ -4,7 +4,4 @@ def call(Map params) {
    
     def scm = checkout scmGit(branches: [[name: "${params.branch}"]], extensions: [], userRemoteConfigs: [[credentialsId: 'just-key', url: "${params.repo}"]])
 
-    //checkout([$class: 'GitSCM', branches: [[name: "${params.branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${params.repo}"]]])
-    //checkout scmGit(branches: [[name: "${params.branch}"]], extensions: [], userRemoteConfigs: [[credentialsId: 'my-key', url: ""${params.repo}"]])
-    // Additional logic can be added here if needed
 }
