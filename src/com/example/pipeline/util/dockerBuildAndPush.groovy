880a5f75670cbada1c3aea6449211bbdd102ce09
shank <shashank3419@gmail.com>
2023-09-06 19:10:13 +1200

    add-lib

--- src/com/example/pipeline/util/dockerBuildAndPush.groovy
+++ src/com/example/pipeline/util/dockerBuildAndPush.groovy
@@ -5,7 +5,7 @@ def call(Map params) {
                  sh """ 
                     aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 193566561588.dkr.ecr.ap-southeast-2.amazonaws.com
                     docker build -t test-dx:${params.version} .
-                    if [ ${params.version} == 'dev' ] ; then
+                    if [ ${params.environment} == 'dev' ] ; then
                     docker tag test-dx:${params.version} 193566561588.dkr.ecr.ap-southeast-2.amazonaws.com/test-dx:${params.version}
                     docker push 193566561588.dkr.ecr.ap-southeast-2.amazonaws.com/test-dx:${env.version}
                     else 
