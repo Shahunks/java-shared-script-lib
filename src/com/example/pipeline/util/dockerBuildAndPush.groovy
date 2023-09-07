ca7bd33ed81e852d0c06c25db627442af592b076
shank <shashank3419@gmail.com>
2023-09-07 13:27:46 +1200

    add-lib

--- src/com/example/pipeline/util/dockerBuildAndPush.groovy
+++ src/com/example/pipeline/util/dockerBuildAndPush.groovy
@@ -17,10 +17,13 @@ package com.example.pipeline.util
 // }
 def call(Map params) {
 
-                 sh """ 
-                    aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 193566561588.dkr.ecr.ap-southeast-2.amazonaws.com
-                    docker buildx create --use 
-                    docker buildx build --platform linux/amd64 --push -t 193566561588.dkr.ecr.ap-southeast-2.amazonaws.com/test-dx:${params.version} .
-                    """
+
+               sh 'echo ${params.version}'
+               //   sh """ 
+               //      aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 193566561588.dkr.ecr.ap-southeast-2.amazonaws.com
+                    
+               //      #docker buildx create --use 
+               //      #docker buildx build --platform linux/amd64 --push -t 193566561588.dkr.ecr.ap-southeast-2.amazonaws.com/test-dx:${params.version} .
+               //      """
 
 }
