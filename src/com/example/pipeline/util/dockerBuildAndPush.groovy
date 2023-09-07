70dd32065af5e5718a1f6b2c251b36949367d094
shank <shashank3419@gmail.com>
2023-09-07 13:29:39 +1200

    add-lib

--- src/com/example/pipeline/util/dockerBuildAndPush.groovy
+++ src/com/example/pipeline/util/dockerBuildAndPush.groovy
@@ -18,7 +18,7 @@ package com.example.pipeline.util
 def call(Map params) {
 
 
-               sh 'echo ${params.version}'
+               sh 'echo $params.version'
                //   sh """ 
                //      aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 193566561588.dkr.ecr.ap-southeast-2.amazonaws.com
                     
